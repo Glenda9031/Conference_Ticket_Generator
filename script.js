42de8257f6027b29624e31e5be0adf98c125610c
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2025-01-27 15:49:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -195,5 +195,10 @@ function getQueryParams() {
     }
 
     const ticketNumber = document.getElementById('ticket-number');
-    
+    let n1 = Math.floor(Math.random() * 10);
+    let n2 = Math.floor(Math.random() * 10);
+    let n3 = Math.floor(Math.random() * 10);
+    let n4 = Math.floor(Math.random() * 10);
+    let n5 = Math.floor(Math.random() * 10);
+    ticketNumber.textContent = `#${n1}${n2}${n3}${n4}${n5}`;
 }
